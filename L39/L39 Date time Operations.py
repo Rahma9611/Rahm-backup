ca319c5fad7c24ef2fa3c94da029ddd415d8d8c8
Rahma9611 <rahmatasneem7@gmail.com>
2025-11-29 13:32:38 +0300

    L39

---
+++ L39/L39 Date time Operations.py
@@ -0,0 +1,13 @@
+import datetime
+
+# using now() to get current time
+current_time = datetime.datetime.now()
+
+# Printing value of now.
+print("Time now at greenwich meridian is : ", end = "")
+print(current_time)
+
+# print caldendar of year 2025
+import calendar
+
+print("\n", calendar.calendar(2025))
