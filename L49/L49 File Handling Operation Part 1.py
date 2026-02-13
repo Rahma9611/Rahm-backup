b312b76719c7aea6e9f8bd950e4d2734d204f856
Rahma9611 <rahmatasneem7@gmail.com>
2026-02-13 13:35:20 +0300

    L49

---
+++ L49/L49 File Handling Operation Part 1.py
@@ -0,0 +1,10 @@
+# open file and store object in a variable
+file = open("L49/Test.txt") #file path from the root of the coding folder
+
+# read the contents of the file
+print(file.read())
+
+print()
+
+# close the file
+file.close()
