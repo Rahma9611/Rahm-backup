1904b5c43d6eee998afac3133dfb37f55b8821db
Rahma9611 <rahmatasneem7@gmail.com>
2026-03-13 13:29:28 +0300

    L56

---
+++ L56/L56 O1 Time.py
@@ -0,0 +1,11 @@
+def printnumber(n):
+    literation = 0
+    print("the total number enteed my user is ", n)
+
+    literation += 1
+    print("total iteration done by computer is ", literation)
+    print()
+
+printnumber(5)
+printnumber(350)
+print("with any 'n the time taken by the computer will not change ")
