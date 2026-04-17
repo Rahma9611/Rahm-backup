5d8a2f1bd06f4e57fb5869604773dd833d3556c8
Rahma9611 <rahmatasneem7@gmail.com>
2026-04-17 13:35:01 +0300

    L64

---
+++ L64/L64/L64 Power of 4.py
@@ -0,0 +1,22 @@
+def powerOf4(n):
+    count = 0
+
+    # If only 1 set bit exists
+    if (n & (~(n & (n-1)))) :
+        # Count  0 bits before set bit 
+        while n > 1:
+            n >>= 1
+            count += 1
+
+    # If count is even retuen ture ture else flase
+    if(count & 2 == 0):
+        return True
+    else:
+        return False
+    
+numb = int(input("Enter a number: "))
+
+if powerOf4(numb):
+    print(f"{numb} is a power of 4.")
+else:
+    print(f"{numb} is not a power of 4.")
