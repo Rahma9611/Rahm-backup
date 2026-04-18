10da61c641c643454651971ebaa5d8478d98a0ba
Rahma9611 <rahmatasneem7@gmail.com>
2026-04-18 12:54:56 +0300

    L57

---
+++ L57/L57 hw.py
@@ -0,0 +1,11 @@
+def timeComplexRecur(n):
+    if n <= 0:
+        return
+    
+    print("test")
+
+    if n >= 2:
+        timeComplexRecur(n/2)
+        timeComplexRecur(n/2)
+
+timeComplexRecur(8)
